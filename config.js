55c5393b1b3f0b52bdf2413b60e9f8979d98c9de
Andrey <novitskiyas@gmail.com>
2025-11-18 18:06:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // GitHub Configuration
 const GITHUB_CONFIG = {
-    TOKEN: 'your_github_token_here', // REPLACE with your token
-    REPO_OWNER: 'your_github_username', // REPLACE with your username
-    REPO_NAME: 'your_repository_name' // REPLACE with your repository name
+    TOKEN: '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', // ВСТАВЬ свой токен с шага 1
+    REPO_OWNER: 'LunaMe0611', // ВСТАВЬ свой username GitHub
+    REPO_NAME: 'GalleryGermanHouse' // ВСТАВЬ название своего репозитория
 };
