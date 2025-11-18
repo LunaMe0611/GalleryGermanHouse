f001e4070a635682d3ad20f31ebd6cffa292e0e0
Andrey <novitskiyas@gmail.com>
2025-11-18 17:56:00 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const GITHUB_CONFIG = {
+    TOKEN: 'твой_github_token', // который ты создал в шаге 1
+    REPO_OWNER: 'твой_github_username', // твой логин GitHub
+    REPO_NAME: 'gallery-site' // название репозитория
+};
