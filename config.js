cb79198d85bb68abad8c96b3f7960d576f15af8a
Andrey <novitskiyas@gmail.com>
2025-11-21 17:29:05 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // GitHub Configuration
 const GITHUB_CONFIG = {
-    TOKEN: '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', // ВСТАВЬ свой токен с шага 1
+    TOKEN: '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', // ВСТАВЬ свой токен с шага 1
     GIST_ID: 'ac6449c25e838f398c7de89169367dc9'
 };
