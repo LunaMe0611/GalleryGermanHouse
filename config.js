4de8a867d7ca13800e44c16b6f98dbb5ad8ae7e9
Andrey <novitskiyas@gmail.com>
2025-11-24 11:24:06 +0300

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// GitHub Configuration
-const GITHUB_CONFIG = {
-    TOKEN: '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', // ВСТАВЬ свой токен с шага 1
-    GIST_ID: 'ac6449c25e838f398c7de89169367dc9'
-};
